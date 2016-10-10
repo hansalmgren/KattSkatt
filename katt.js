07dda71351fea837469f387e270b9ce3a345594e
hal <hal@enigio.com>
2016-10-10 16:49:52 +0200

    test

--- katt.js
+++ katt.js
@@ -1,12 +1,19 @@
 
+'use strict';
+
 /**
  * Beräknar & meddelar dagens Katt-skatt
  *
- */
+ *
 module.exports = {
   printMsg: function() {
 	  console.log("Dagens KattSkatt är 56%.");
   }
-};
+};*/
 
 
+exports.printMsg = function() {
+	  console.log("This is a message from the demo package");
+	}
+
+console.log("ssws");
