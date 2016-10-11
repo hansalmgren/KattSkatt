e978493bad60630d5f40eec14ed03bcb263d33fa
Hans Almgren <hal@enigio.com>
2016-10-11 02:07:26 +0200

    working ok

---
+++ bin/kattskatt.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+// -*- js -*-
+
+"use strict";
+
+var kattskatt = require("kattskatt");
+var percent = kattskatt();
+
+console.log('Dagens KattSkatt är: '+percent+'%.');
--- katt.js
+++ katt.js
@@ -1,19 +1,11 @@
-
 'use strict';
 
 /**
  * Beräknar & meddelar dagens Katt-skatt
- *
- *
-module.exports = {
-  printMsg: function() {
-	  console.log("Dagens KattSkatt är 56%.");
-  }
-};*/
+ * 
+ */
 
+module.exports = function() {
+	return Math.floor(Math.random()*1000)/100;
+};
 
-exports.printMsg = function() {
-	  console.log("This is a message from the demo package");
-	}
-
-console.log("ssws");
